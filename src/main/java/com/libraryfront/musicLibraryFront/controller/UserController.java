e089cbc4fdba33230303713e27edfd799f60926e
Med <mehdi.gacem@versusmind.eu>
2018-06-25 11:47:58 +0200

    Spring front music library front application consuming the musicLibraryAPI

---
+++ src/main/java/com/libraryfront/musicLibraryFront/controller/UserController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.libraryfront.musicLibraryFront.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Mehdi
+ */
+@Controller
+public class UserController 
+{
+}
