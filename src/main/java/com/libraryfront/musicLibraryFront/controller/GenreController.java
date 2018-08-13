406576a5b64050502cbf75b2ec12421a5a64266e
Med <mehdi.gacem@versusmind.eu>
2018-08-13 18:01:14 +0200

    Remove update genre form initialisation after update form submission

--- src/main/java/com/libraryfront/musicLibraryFront/controller/GenreController.java
+++ src/main/java/com/libraryfront/musicLibraryFront/controller/GenreController.java
@@ -168,8 +168,8 @@ public class GenreController
         if(bindingResult.hasErrors())
         {
             //Initialze the Genre Update Form again
-            GenreDTO genreFound = genreService.findGenreById(restTemplate, id);
-            genreDTOComponent.iniGenreDTO(genreDTO, genreFound);
+            //GenreDTO genreFound = genreService.findGenreById(restTemplate, id);
+            //genreDTOComponent.iniGenreDTO(genreDTO, genreFound);
             return "musicGenre/genreUpdateForm";
         }
         
