ee94e9c7fdcde1930bab5509f4332f9745f0ee60
Med <mehdi.gacem@versusmind.eu>
2018-08-17 17:04:22 +0200

    Create controller method to bring back a category by its id

--- src/main/java/com/libraryfront/musicLibraryFront/controller/CategoryController.java
+++ src/main/java/com/libraryfront/musicLibraryFront/controller/CategoryController.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -32,5 +34,15 @@ public class CategoryController
         modelMap.addAttribute("categoryList", categoryList);
         
         return "musicCategory/categoryList";
+    }
+    
+    @GetMapping("/categories/{id}")
+    public ModelAndView showSpecificCategory(@PathVariable(name = "id") long categoryId, RestTemplate restTemplate)
+    {
+        CategoryDTO foundCategory = categoryService.findCategoryById(restTemplate, categoryId);
+        ModelAndView modelAndView = new ModelAndView("musicCategory/specificCategory");
+        modelAndView.addObject("foundCategory", foundCategory);
+        
+        return modelAndView;
     }
 }
