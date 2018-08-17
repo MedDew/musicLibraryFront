8b5ec37f3f913382ea07ef2cc34b7ed6517ff092
Med <mehdi.gacem@versusmind.eu>
2018-08-17 16:33:44 +0200

    Create service getting the category list by establishin a communication to the rest webservice through restemplate

---
+++ src/main/java/com/libraryfront/musicLibraryFront/service/CategoryService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.libraryfront.musicLibraryFront.service;
+
+import com.musiclibrary.musiclibraryapi.dto.CategoryDTO;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Mehdi
+ */
+@Service
+public class CategoryService 
+{
+   private static final String URL_GET_CATEGORIES = "http://localhost:8090/categories";
+   private static final String URL_GET_CATEGORY = "http://localhost:8090/categories/{id}";
+   private static final String URL_CREATE_CATEGORY = "http://localhost:8090/categories/create";
+   private static final String URL_UPDATE_CATEGORY = "http://localhost:8090/categories/update/{id}";
+   private static final String URL_DELETE_CATEGORY = "http://localhost:8090/categories/delete/{id}";
+   
+   public List<CategoryDTO> getCategoryList(RestTemplate restTemplate)
+   {
+       List<CategoryDTO> categoryList = restTemplate.getForObject(URL_GET_CATEGORIES, ArrayList.class);
+       
+       return categoryList;
+   }
+}
