249d6ebc84a26067d2c451f541474f5ffd964066
Med <mehdi.gacem@versusmind.eu>
2018-08-20 09:32:29 +0200

    Create method to display the category creation form

--- src/main/java/com/libraryfront/musicLibraryFront/controller/CategoryController.java
+++ src/main/java/com/libraryfront/musicLibraryFront/controller/CategoryController.java
@@ -8,6 +8,9 @@ package com.libraryfront.musicLibraryFront.controller;
 import com.libraryfront.musicLibraryFront.service.CategoryService;
 import com.musiclibrary.musiclibraryapi.dto.CategoryDTO;
 import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 /**
  *
@@ -43,6 +47,14 @@ public class CategoryController
         ModelAndView modelAndView = new ModelAndView("musicCategory/specificCategory");
         modelAndView.addObject("foundCategory", foundCategory);
         
+        return modelAndView;
+    }
+    
+    @GetMapping("/categories/create")
+    public ModelAndView showCategoryForm(CategoryDTO categoryDTO)
+    {
+//        View view = 
+        ModelAndView modelAndView = new ModelAndView("musicCategory/categoryCreateForm");
         return modelAndView;
     }
 }
