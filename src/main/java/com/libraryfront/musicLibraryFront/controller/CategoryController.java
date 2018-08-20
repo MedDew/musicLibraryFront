e3f642c188b3e593fbd8aba1f1fa178f2506a248
Med <mehdi.gacem@versusmind.eu>
2018-08-20 10:22:03 +0200

    Create method to post a new category + Create method to show the result of the created category

--- src/main/java/com/libraryfront/musicLibraryFront/controller/CategoryController.java
+++ src/main/java/com/libraryfront/musicLibraryFront/controller/CategoryController.java
@@ -8,17 +8,16 @@ package com.libraryfront.musicLibraryFront.controller;
 import com.libraryfront.musicLibraryFront.service.CategoryService;
 import com.musiclibrary.musiclibraryapi.dto.CategoryDTO;
 import java.util.List;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 /**
  *
@@ -55,6 +54,29 @@ public class CategoryController
     {
 //        View view = 
         ModelAndView modelAndView = new ModelAndView("musicCategory/categoryCreateForm");
+        return modelAndView;
+    }
+    
+    @PostMapping("/categories/create")
+    public String postCategory(@Valid CategoryDTO categoryDTO, BindingResult bindingResult, RestTemplate restTemplate, ModelMap modelMap)
+    {
+        if(bindingResult.hasErrors())
+        {
+            return "musicCategory/categoryCreateForm";
+        }
+        
+        CategoryDTO createdCategory = categoryService.createCategory(restTemplate, categoryDTO);
+        
+        return "redirect:/category/created/"+createdCategory.getId();
+    }
+    
+    @GetMapping(path = "/category/created/{categoryId}")
+    public ModelAndView showCategoryCreated(RestTemplate restTemplate, @PathVariable long categoryId)
+    {
+        CategoryDTO createdCategory = categoryService.findCategoryById(restTemplate, categoryId);
+        ModelAndView modelAndView = new ModelAndView("/musicCategory/createdCategory");
+        modelAndView.addObject("category", createdCategory);
+        
         return modelAndView;
     }
 }
