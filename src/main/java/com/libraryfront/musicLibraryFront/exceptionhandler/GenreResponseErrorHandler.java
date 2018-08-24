cde90d650d83d956716c53b3e2303ef2bc71c501
Med <mehdi.gacem@versusmind.eu>
2018-08-24 12:13:55 +0200

    Modify the handleError method to use the factory creational pattern and insytnatiate the right exception acoording to the error message

--- src/main/java/com/libraryfront/musicLibraryFront/exceptionhandler/GenreResponseErrorHandler.java
+++ src/main/java/com/libraryfront/musicLibraryFront/exceptionhandler/GenreResponseErrorHandler.java
@@ -5,13 +5,19 @@
  */
 package com.libraryfront.musicLibraryFront.exceptionhandler;
 
-import com.libraryfront.musicLibraryFront.exception.GenreException;
+import com.libraryfront.musicLibraryFront.Enum.ExceptionType;
+import com.libraryfront.musicLibraryFront.exception.Exception;
+import com.libraryfront.musicLibraryFront.factory.Factory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.ResponseErrorHandler;
@@ -23,7 +29,7 @@ import org.springframework.web.client.ResponseErrorHandler;
 public class GenreResponseErrorHandler implements ResponseErrorHandler
 {
     private ResponseErrorHandler errorHandler = new DefaultResponseErrorHandler();
-
+    
     @Override
     public boolean hasError(ClientHttpResponse response) throws IOException 
     {
@@ -35,15 +41,36 @@ public class GenreResponseErrorHandler implements ResponseErrorHandler
     {
         //Convert an InputStream to String
         String responseBody =  new BufferedReader(new InputStreamReader(response.getBody())).lines().collect(Collectors.joining("\n"));
-        GenreException genreException = new GenreException(response.getStatusCode());
+        //Exception genreException = new Exception(response.getStatusCode());
+        
+        //RECOVER VALUES OF THE EXCEPTION TYPE TO SEARCH IN
+        //BY FILTERING  IN THE ENUM ACCORDING TO THE responseBody
+        ExceptionType.getExceptionTypeDefaultList().stream().forEach((ex) -> System.out.println("ExceptionType : "+ex+" | ex.name() : "+ex.name()+" "+ex.name().getClass().getSimpleName()+" | ex.toString() : "+ex.toString()+" "+ex.toString().getClass().getSimpleName()+" | ExceptionType Type : "+ex.getClass().getSimpleName()) );
+        JSONObject jsonResponseBody = new JSONObject(responseBody);
+        String message = (String) jsonResponseBody.get("message");
+        String str1 = "CATGEORY";
+        String str2 = "Category";
+        boolean isStr1 = message.contains(str1);
+        boolean isStr2 = message.contains(str2);
+        
+        //FIND THE RIGHT ExecptionType FROM THE message BY FILTERING AMONG THE Enum DEFAULT ExceptionType
+        System.out.println("isStr1 : "+isStr1+" isStr2 : "+isStr2);
+        Optional<ExceptionType> exceptionType = ExceptionType.getExceptionTypeDefaultList().stream().filter((ex) -> message.contains(ex.toString())).findAny();
+        
+        //RECOVER THE RIGHT ExceptionType TO USE
+        Factory factory = new Factory();
+        Exception exceptionToUse =  factory.getException(exceptionType.get(), response.getStatusCode());
+        
         
         Map<String, Object> responseProperties = new HashMap<>();
         responseProperties.put("status", response.getStatusCode().toString());
         responseProperties.put("body", responseBody);
         responseProperties.put("headers", response.getHeaders());
         
-        genreException.setResponse(responseProperties);
+        //genreException.setResponse(responseProperties);
+        exceptionToUse.setResponse(responseProperties);
         
-        throw genreException;
+//        throw genreException;
+        throw exceptionToUse;
     }
 }
